df685969ac4531a465ab039e1736b6ed2e8e6463
bertdk <bertdk10@gmail.com>
2021-11-10 00:10:05 +0100

    fix: Body set validation middleware on method

--- src/decorators/body.decorator.ts
+++ src/decorators/body.decorator.ts
@@ -1,10 +1,14 @@
-import { Body as BodyRoutingControllers, BodyOptions, UseBefore } from 'routing-controllers';
+import { Body as BodyRoutingControllers, BodyOptions, getMetadataArgsStorage } from 'routing-controllers';
 import { Newable, validationMiddleware } from 'utils';
 
 export const Body = (type: Newable, bodyOptions?: BodyOptions) => {
-  return (target: Object, propertyKey: string | symbol, parameterIndex: number) => {
-    [(UseBefore(validationMiddleware(type, 'body')), BodyRoutingControllers(bodyOptions))].map(f =>
-      f(target, propertyKey, parameterIndex),
-    );
+  return (target: Object, propertyKey: string, parameterIndex: number) => {
+    getMetadataArgsStorage().uses.push({
+      target: target.constructor,
+      method: propertyKey,
+      middleware: validationMiddleware(type, 'body'),
+      afterAction: false,
+    });
+    [BodyRoutingControllers(bodyOptions)].map(f => f(target, propertyKey, parameterIndex));
   };
 };
