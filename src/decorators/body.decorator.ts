af551d9b6c1629180378aa04cd1dda841bd1bd61
bertdk <bertdk10@gmail.com>
2021-11-10 02:48:45 +0100

    fix: refactor body decorator (useBefore)

--- src/decorators/body.decorator.ts
+++ src/decorators/body.decorator.ts
@@ -1,14 +1,9 @@
-import { Body as BodyRoutingControllers, BodyOptions, getMetadataArgsStorage } from 'routing-controllers';
+import { Body as BodyRoutingControllers, BodyOptions, UseBefore } from 'routing-controllers';
 import { Newable, validationMiddleware } from 'utils';
 
 export const Body = (type: Newable, bodyOptions?: BodyOptions) => {
   return (target: Object, propertyKey: string, parameterIndex: number) => {
-    getMetadataArgsStorage().uses.push({
-      target: target.constructor,
-      method: propertyKey,
-      middleware: validationMiddleware(type, 'body'),
-      afterAction: false,
-    });
-    [BodyRoutingControllers(bodyOptions)].map(f => f(target, propertyKey, parameterIndex));
+    UseBefore(validationMiddleware(type, 'body'))(target, propertyKey);
+    BodyRoutingControllers(bodyOptions)(target, propertyKey, parameterIndex);
   };
 };
