4164ed09114f857958dea80b28235a4655bc9b6b
Leslie Jobse <leslie@jobse.be>
2021-11-13 11:24:08 +0100

    fix: any requirement

--- src/authorization/handleRequirements.ts
+++ src/authorization/handleRequirements.ts
@@ -1,9 +1,28 @@
 import { Request } from 'express';
 
-import { promiseAny } from './promiseAny';
+import { Forbidden } from '../contracts/errors/forbidden.error';
 import { IRequirement } from './requirement.interface';
 
 export const handleEitherRequirements = async (requirements: IRequirement | IRequirement[], request: Request) => {
   const list = Array.isArray(requirements) ? requirements : [requirements];
-  await promiseAny(list.map(async requirement => requirement(request)));
+
+  const functionsToValidate = list.map(requirement => () => requirement(request));
+  await promiseAny(functionsToValidate);
+};
+
+const promiseAny = async (functionsToValidate: ((() => Promise<void>) | (() => void))[]) => {
+  const errors = [];
+
+  await new Promise((res, rej) => {
+    functionsToValidate.map(async validationFunc => {
+      try {
+        const v = await validationFunc();
+        res(v);
+      } catch (error) {
+        errors.push(error);
+        if (errors.length !== functionsToValidate.length) return;
+        rej(new Forbidden('forbidden', errors.map(e => e.reason).join(', ')));
+      }
+    });
+  });
 };
