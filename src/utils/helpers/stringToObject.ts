fa2a495d3922ff1097e8c9fa1f0140a45514b1bb
bertdk <bertdk10@gmail.com>
2021-11-16 11:24:00 +0100

    fix: stringToObject

--- src/utils/helpers/stringToObject.ts
+++ src/utils/helpers/stringToObject.ts
@@ -1,11 +1,12 @@
 export const stringToObject = (path: string, value: any, obj: object = {}) => {
-  let parts = path.split('.'),
-    part: string;
-  let last = parts.pop();
+  const result = {};
+  const parts = path.split('.');
+  let part: string;
+  const last = parts.pop();
   while ((part = parts.shift())) {
-    if (typeof obj[part] != 'object') obj[part] = {};
+    if (typeof obj[part] !== 'object') obj[part] = {};
     obj = obj[part]; // update "pointer"
   }
   obj[last] = value;
-  return obj;
+  return result;
 };
