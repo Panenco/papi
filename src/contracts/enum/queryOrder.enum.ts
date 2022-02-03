e3b4a0a57325235739c32cf157e28149686cb124
bertdk <bertdk10@gmail.com>
2022-02-03 18:26:57 +0100

    fix: query order enum

--- src/contracts/enum/queryOrder.enum.ts
+++ src/contracts/enum/queryOrder.enum.ts
@@ -1,4 +1,7 @@
-export declare enum QueryOrder {
+/**
+ * Capitalized query order enum
+ */
+export enum QueryOrder {
   ASC = 'ASC',
   ASC_NULLS_LAST = 'ASC NULLS LAST',
   ASC_NULLS_FIRST = 'ASC NULLS FIRST',
