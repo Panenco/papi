76f95b7215587ea63aa1fdf0b22b0a44586e74e6
bertdk <bertdk10@gmail.com>
2022-02-25 09:02:46 +0100

    feat: lower transform decorator

--- src/decorators/transformLower.decorator.ts
+++ src/decorators/transformLower.decorator.ts
@@ -1,5 +1,5 @@
 import { Transform } from 'class-transformer';
 
 export const Lower = () => {
-  return Transform(({ value }) => value.toLowerCase());
+  return Transform(({ value }) => value?.toLowerCase());
 };
